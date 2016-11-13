9a409eaaccda81afb07f9aeb2f4781b35071afe1
guanyuan <yuanguan89@gmail.com>
2016-11-13 23:56:03 +0800

    链接没有箭头

--- js/basic.js
+++ js/basic.js
@@ -4,38 +4,35 @@ var diagram = G(go.Diagram, "myDiagramDiv");
 diagram.nodeTemplate = G(
   go.Node,
   'Auto',
-  new go.Binding('location', 'loc', go.Point.parse),
+  new go.Binding('location', 'loc', go.Point.Parse),
   G(
     go.Shape,
     'RoundedRectangle',
-    { fill: '#fff' },
-    new go.Binding('fill', 'color')
+    {
+      fill: '#ddd'
+    }
   ),
   G(
     go.TextBlock,
-    new go.Binding('text', 'text')
+    {
+      margin: 5
+    },
+    new go.Binding('text', 'key')
   )
-)
+);
 diagram.linkTemplate = G(
   go.Link,
   G(
-    go.Shape,
-    new go.Binding('stroke', 'color'),
-    new go.Binding('strokeWidth', 'thick')
-  ),
-  G(
-    go.Shape,
-    { toArrow: 'OpenTriangle', fill: null}
+    go.Shape
   )
-)
+);
 var nodeDataArray = [
-  { key:'1', text: 'hello1', color: '#345', loc: '0, 0'},
-  { key:'2', text: 'hello2', color: '#245', loc: '0, 150'},
-  { key:'3', text: 'hello3', loc: '0, 250'}
-]
+  { key: 'apple', loc: '0 0'},
+  { key: 'object-c', loc: '0 150'}
+];
+
 var linkDataArray = [
-  {from: '1', to: '2', color: '#452', thick: 2},
-  {from: '2', to: '3', color: '#452', thick: 2},
-]
+  { from: 'apple', to: 'object-c'}
+];
 
 diagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
