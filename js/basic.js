96dd89d766bfb2203496104223d5a8b53ef150e6
guanyuan <yuanguan89@gmail.com>
2016-11-13 23:41:59 +0800

    简单的节点和链接

--- js/basic.js
+++ js/basic.js
@@ -1,85 +1,47 @@
 var G = go.GraphObject.make;
 var diagram = G(go.Diagram, "myDiagramDiv");
 
-diagram.add(G(
-  go.Part,
+var node1 = G(
+  go.Node,
+  'Auto',
   G(
-    go.Panel,
-    'Table',
-    G(
-      go.TextBlock,
-      {
-        text: '顶标题',
-        row: 0,
-        column: 0,
-        columnSpan: 3,
-        stretch: go.GraphObject.Horizontal,
-        margin: 2,
-        background: '#394'
-      }
-    ),
-    G(
-      go.TextBlock,
-      {
-        text: '侧标题',
-        row: 1,
-        column: 0,
-        rowSpan:2,
-        margin: 2,
-        stretch: go.GraphObject.Vertical,
-        background: '#394'
-      }
-    ),
-    G(
-      go.TextBlock,
-      {
-        text: '一行一列',
-        row: 1,
-        column: 1,
-        margin: 2,
-        background: '#394'
-      }
-    ),
-    G(
-      go.TextBlock,
-      {
-        text: '一行两列',
-        row: 1,
-        column: 2,
-        margin: 2,
-        background: '#394'
-      }
-    ),
-    G(
-      go.TextBlock,
-      {
-        text: '二行一列',
-        row: 2,
-        column: 1,
-        margin: 2,
-        background: '#394'
-      }
-    ),
-    G(
-      go.TextBlock,
-      {
-        text: '二行三列',
-        row: 2,
-        column: 3,
-        margin: 2,
-        background: '#394'
-      }
-    ),
-    G(
-      go.TextBlock,
-      {
-        text: '结尾',
-        row: 3,
-        column: 2,
-        columnSpan: 2,
-        margin: 2,
-        background: '#394'
-      }
-    )
+    go.Shape,
+    {
+      fill: '#493'
+    }
+  ),
+  G(
+    go.TextBlock,
+    {
+      text: 'node1'
+    }
+  )
+);
+diagram.add(node1);
+var node2 = G(
+  go.Node,
+  'Auto',
+  G(
+    go.Shape,
+    {
+      fill: '#943'
+    }
+  ),
+  G(
+    go.TextBlock,
+    {
+      text: 'node2'
+    }
+  )
+);
+diagram.add(node2);
+diagram.add(G(
+  go.Link,
+  {
+    fromNode: node1,
+    toNode: node2
+  },
+  G(
+    go.Shape
   )
 ));
