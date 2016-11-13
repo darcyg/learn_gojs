a9a8ecf80b88d74e4003efc32609c18ad294ecc0
guanyuan <yuanguan89@gmail.com>
2016-11-13 23:37:37 +0800

    排列和对齐: 按行对齐

--- js/basic.js
+++ js/basic.js
@@ -3,24 +3,109 @@ var diagram = G(go.Diagram, "myDiagramDiv");
 
 diagram.add(G(
   go.Part,
-  'Table',
-  {
-    background: '#ddd'
-  },
   G(
-    go.TextBlock,
+    go.Panel,
+    'Table',
     {
-      text: '这里的文字会重叠',
-      row: 0,
-      column: 0
-    }
-  ),
-  G(
-    go.TextBlock,
-    {
-      text: '叠重会字文的里这',
-      row: 0,
-      column: 0
-    }
+      defaultAlignment: go.Spot.Left
+    },
+    G(
+      go.RowColumnDefinition,
+      {
+        column: 0,
+        width: 100
+      }
+    ),
+    G(
+      go.RowColumnDefinition,
+      {
+        column:1,
+        width: 15,
+      }
+    ),
+    G(
+      go.Panel,
+      'Auto',
+      {
+        row: 0,
+        column: 0,
+        alignment: go.Spot.Left
+      },
+      G(
+        go.Shape,
+        'RoundedRectangle',
+        {
+          fill: '#493'
+        }
+      ),
+      G(
+        go.TextBlock,
+        'auot panel'
+      )
+    ),
+    G(
+      go.TextBlock,
+      {
+        text: 'alignment: left',
+        row: 0,
+        column:2
+      }
+    ),
+    G(
+      go.Panel,
+      'Auto',
+      {
+        row: 1,
+        column: 0,
+        alignment: go.Spot.Center
+      },
+      G(
+        go.Shape,
+        'RoundedRectangle',
+        {
+          fill: '#493'
+        }
+      ),
+      G(
+        go.TextBlock,
+        'auto panel'
+      )
+    ),
+    G(
+      go.TextBlock,
+      {
+        text: 'alignment: center',
+        row:1,
+        column: 2
+      }
+    ),
+    G(
+      go.Panel,
+      'Auto',
+      {
+        row:2,
+        column: 0,
+        alignment: go.Spot.Right
+      },
+      G(
+        go.Shape,
+        'RoundedRectangle',
+        {
+          fill: '#493'
+        }
+      ),
+      G(
+        go.TextBlock,
+        'auto panel'
+      )
+    ),
+    G(
+      go.TextBlock,
+      {
+        text: 'alignment: right',
+        row: 2,
+        column: 2
+      }
+    )
   )
 ));
