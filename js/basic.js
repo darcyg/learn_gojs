dbd7e15ac789e5a2d3057d6d86024087b7f301f6
guanyuan <yuanguan89@gmail.com>
2016-11-13 22:41:35 +0800

    Panel: 位置面板

--- js/basic.js
+++ js/basic.js
@@ -1,40 +1,43 @@
 var G = go.GraphObject.make;
 var diagram = G(go.Diagram, "myDiagramDiv");
 
-var style = {
-  width: 55,
-  height: 30,
-  margin: 5,
-  fill: '#555'
-}
 diagram.add(G(
-  go.Node,
-  'Auto',
-  G(
-    go.Shape,
-    'RoundedRectangle',
-    style
-  ),
+  go.Part,
+  go.Panel.Position,
+  //最简单的一种面板是“Position”（Panel.Position）。每个元素获得其正常大小，无论其默认大小或指定GraphObject.desiredSize
+  //面板的大小刚好足以容纳所有元素
+  {
+    background: '#eee'
+  },
   G(
     go.TextBlock,
     {
-      text: 'xy1'
+      text: '0, 0',
+      background: '#394'
     }
-  )
-));
-
-diagram.add(G(
-  go.Node,
-  'Auto',
-  G(
-    go.Shape,
-    'Rectangle',
-    style
   ),
   G(
     go.TextBlock,
     {
-      text: 'xy2'
+      text: '100, 100',
+      background: '#394',
+      position: new go.Point(100, 100)
+    }
+  ),
+  G(
+    go.TextBlock,
+    {
+      text: '0, 100',
+      background: '#394',
+      position: new go.Point(0, 100)
+    }
+  ),
+  G(
+    go.TextBlock,
+    {
+      text: '100, 000',
+      background: '#394',
+      position: new go.Point(100, 0)
     }
   )
 ));
