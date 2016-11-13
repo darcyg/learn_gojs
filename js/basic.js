302404d6c88012018464ab256f96977474c9d851
guanyuan <yuanguan89@gmail.com>
2016-11-13 23:53:18 +0800

    Point.parse 解析

--- js/basic.js
+++ js/basic.js
@@ -4,7 +4,7 @@ var diagram = G(go.Diagram, "myDiagramDiv");
 diagram.nodeTemplate = G(
   go.Node,
   'Auto',
-  new go.Binding('location', 'loc'),
+  new go.Binding('location', 'loc', go.Point.parse),
   G(
     go.Shape,
     'RoundedRectangle',
@@ -29,12 +29,13 @@ diagram.linkTemplate = G(
   )
 )
 var nodeDataArray = [
-  { key:'1', text: 'hello1', color: '#345', loc: new go.Point(0, 0)},
-  { key:'2', text: 'hello2', color: '#245', loc: new go.Point(0, 100)},
-  { key:'3', text: 'hello3', loc: new go.Point(0, 200)}
+  { key:'1', text: 'hello1', color: '#345', loc: '0, 0'},
+  { key:'2', text: 'hello2', color: '#245', loc: '0, 150'},
+  { key:'3', text: 'hello3', loc: '0, 250'}
 ]
 var linkDataArray = [
   {from: '1', to: '2', color: '#452', thick: 2},
   {from: '2', to: '3', color: '#452', thick: 2},
 ]
+
 diagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
