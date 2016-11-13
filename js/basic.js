11d46f8b47b232492729e6429c8e7c2786a965fa
guanyuan <yuanguan89@gmail.com>
2016-11-13 22:31:54 +0800

    GraphObject.make: 公共js变量

--- js/basic.js
+++ js/basic.js
@@ -1,22 +1,40 @@
 var G = go.GraphObject.make;
 var diagram = G(go.Diagram, "myDiagramDiv");
 
+var style = {
+  width: 55,
+  height: 30,
+  margin: 5,
+  fill: '#555'
+}
 diagram.add(G(
   go.Node,
   'Auto',
   G(
     go.Shape,
     'RoundedRectangle',
-    {
-      fill: '#394'
-    }
+    style
   ),
   G(
     go.TextBlock,
-    'Hello!',
     {
-      margin: 5,
-      editable: true
+      text: 'xy1'
+    }
+  )
+));
+
+diagram.add(G(
+  go.Node,
+  'Auto',
+  G(
+    go.Shape,
+    'Rectangle',
+    style
+  ),
+  G(
+    go.TextBlock,
+    {
+      text: 'xy2'
     }
   )
 ));
