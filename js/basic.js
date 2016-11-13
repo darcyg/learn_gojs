07ec9bedf27aa3132c75963f6e269f7e7d3ec6e7
guanyuan <yuanguan89@gmail.com>
2016-11-13 23:48:58 +0800

    绑定node模板和 link模板

--- js/basic.js
+++ js/basic.js
@@ -1,24 +1,39 @@
 var G = go.GraphObject.make;
 var diagram = G(go.Diagram, "myDiagramDiv");
 
-diagram.nodeTemplate =
-  $(go.Node, "Auto",
-    $(go.Shape,
-      { figure: "RoundedRectangle",
-        fill: "white" },
-      //数据绑定是一种声明.
-      //声明一个对象的属性值应该用于设置另一个对象的属性值。
-      new go.Binding("fill", "color")),
-    $(go.TextBlock,
-      { margin: 5 },
-      new go.Binding("text", "key"))
-  );
-
+diagram.nodeTemplate = G(
+  go.Node,
+  'Auto',
+  G(
+    go.Shape,
+    'RoundedRectangle',
+    { fill: '#fff' },
+    new go.Binding('fill', 'color')
+  ),
+  G(
+    go.TextBlock,
+    new go.Binding('text', 'text')
+  )
+)
+diagram.linkTemplate = G(
+  go.Link,
+  G(
+    go.Shape,
+    new go.Binding('stroke', 'color'),
+    new go.Binding('strokeWidth', 'thick')
+  ),
+  G(
+    go.Shape,
+    { toArrow: 'OpenTriangle', fill: null}
+  )
+)
 var nodeDataArray = [
-  { key: "Alpha", color: "lightblue" },
-  { key: "Beta", color: "pink" }
-];
+  { key:'1', text: 'hello1', color: '#345'},
+  { key:'2', text: 'hello2', color: '#245'},
+  { key:'3', text: 'hello3'}
+]
 var linkDataArray = [
-  { from: "Alpha", to: "Beta" }
-];
+  {from: '1', to: '2', color: '#452', thick: 2},
+  {from: '2', to: '3', color: '#452', thick: 2},
+]
 diagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
