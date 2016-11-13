f18656e635298a2aac25faf4d1ac2b22c555acd6
guanyuan <yuanguan89@gmail.com>
2016-11-13 23:43:31 +0800

    节点的模型和模板

--- js/basic.js
+++ js/basic.js
@@ -1,47 +1,21 @@
 var G = go.GraphObject.make;
 var diagram = G(go.Diagram, "myDiagramDiv");
 
-var node1 = G(
-  go.Node,
-  'Auto',
-  G(
-    go.Shape,
-    {
-      fill: '#493'
-    }
-  ),
-  G(
-    go.TextBlock,
-    {
-      text: 'node1'
-    }
-  )
-);
-diagram.add(node1);
-var node2 = G(
-  go.Node,
-  'Auto',
-  G(
-    go.Shape,
-    {
-      fill: '#943'
-    }
-  ),
-  G(
-    go.TextBlock,
-    {
-      text: 'node2'
-    }
-  )
-);
-diagram.add(node2);
-diagram.add(G(
-  go.Link,
-  {
-    fromNode: node1,
-    toNode: node2
-  },
-  G(
-    go.Shape
-  )
-));
+diagram.nodeTemplate =
+  G(go.Node, "Auto",
+    G(go.Shape,
+      { figure: "RoundedRectangle",
+        fill: "white" }),
+    G(go.TextBlock,
+      { text: "hello!",
+        margin: 5 })
+  );
+
+var nodeDataArray = [
+  { key: "Alpha" },
+  { key: "Beta" }
+];
+var linkDataArray = [
+  { from: "Alpha", to: "Beta" }
+];
+diagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
