82f93828bdd5da0e08fee9b11731651f3023fb67
guanyuan <yuanguan89@gmail.com>
2016-11-13 22:58:12 +0800

    Panel: 表面板 can overlap

--- js/basic.js
+++ js/basic.js
@@ -3,47 +3,24 @@ var diagram = G(go.Diagram, "myDiagramDiv");
 
 diagram.add(G(
   go.Part,
-  go.Panel.Table,
-  //表板中的每个对象被放入由的值索引的GraphObject.row和GraphObject.column。
-  //面板会看行和列的所有在面板中的对象，以确定该表应多少行和列。
+  'Table',
+  {
+    background: '#ddd'
+  },
   G(
     go.TextBlock,
     {
-      text: '一行一列',
+      text: '这里的文字会重叠',
       row: 0,
-      column: 0,
-      margin: 2,
-      background: '#493'
+      column: 0
     }
   ),
   G(
     go.TextBlock,
     {
-      text: '一行二列',
+      text: '叠重会字文的里这',
       row: 0,
-      column: 1,
-      margin: 2,
-      background: '#493'
-    }
-  ),
-  G(
-    go.TextBlock,
-    {
-      text: '二行一列',
-      row: 1,
-      column: 0,
-      margin: 2,
-      background: '#493'
-    }
-  ),
-  G(
-    go.TextBlock,
-    {
-      text: '二行三列',
-      row: 1,
-      column: 2,
-      margin: 2,
-      background: '#493'
+      column: 0
     }
   )
 ));
