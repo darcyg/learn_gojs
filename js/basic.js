2a054e50dcd845bdefe8f19cae9d28f403737d06
guanyuan <yuanguan89@gmail.com>
2016-11-13 22:29:46 +0800

    Node: GraphObject.make

--- js/basic.js
+++ js/basic.js
@@ -1,14 +1,22 @@
 var G = go.GraphObject.make;
 var diagram = G(go.Diagram, "myDiagramDiv");
 
-var node = new go.Node(go.Panel.Auto);
-var shape = new go.Shape();
-shape.figure = 'RoundedRectangle';
-shape.fill = '#394';
-node.add(shape);
-var textBlock = new go.TextBlock();
-textBlock.text = 'Hello!';
-textBlock.margin = 5;
-textBlock.editable = true;
-node.add(textBlock);
-diagram.add(node);
+diagram.add(G(  //GraphObject.make是一个函数的第一个参数必须是一个类类型。
+  go.Node,
+  go.Panel.Auto,
+  G(
+    go.Shape,
+    'RoundedRectangle',
+    {
+      fill: '#394'
+    }
+  ),
+  G(
+    go.TextBlock,
+    {
+      text: 'Hello!',
+      margin: 5,
+      editable: true
+    }
+  )
+));
